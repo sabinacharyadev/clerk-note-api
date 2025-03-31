d4842fdfc2f6a0b8dd0870fdc5af08a7fbe3df71
sabinbabu <sabinacharya444@gmail.com>
2025-03-31 16:42:47 +1100

    schema updated

--- schema/noteSchema.js
+++ schema/noteSchema.js
@@ -1,16 +1,21 @@
 import mongoose from "mongoose";
 
-const noteSchema = mongoose.Schema({
-  note: {
-    type: String,
-    required: true,
+const noteSchema = mongoose.Schema(
+  {
+    note: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "user",
+    },
   },
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "user",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const noteModel = mongoose.model("note", noteSchema);
 export default noteModel;
--- schema/userSchema.js
+++ schema/userSchema.js
@@ -1,23 +1,28 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  clerkId: {
-    type: String,
-    required: true,
+const userSchema = mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+    },
+    role: {
+      type: String,
+      default: "buyer",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  name: {
-    type: String,
-  },
-  role: {
-    type: String,
-    default: "buyer",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const userModel = mongoose.model("user", userSchema);
 export default userModel;
